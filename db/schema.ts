de7f5f6b05e06b7098b2935bd447788b5cd95330
ilyholu <ilyholu@gmail.com>
2026-06-01 13:01:56 +0300

    feat: create users table and proceed migration

--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm"
 import { pgTable, integer, serial, text } from "drizzle-orm/pg-core"
 
 export const blogs = pgTable('blogs', {
@@ -6,4 +7,22 @@ export const blogs = pgTable('blogs', {
     author: text('author').notNull(),
     url: text('url').notNull(),
     likes: integer('likes').default(0),
+    userId: integer('user_id').notNull().references(() => users.id)
 })
+
+export const users = pgTable('users', {
+    id: serial('id').primaryKey(),
+    username: text('username').notNull(),
+    name: text('name').notNull(),
+})
+
+export const usersRelations = relations(users, ({ many }) => ({
+    blogs: many(blogs),
+}))
+
+export const blogsRelations = relations(blogs, ({ one }) => ({
+    user: one(users, {
+        fields: [blogs.userId],
+        references: [users.id]
+    })
+}))
